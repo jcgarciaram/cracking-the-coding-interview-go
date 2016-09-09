5edc5fe1f421c73de15dfc9ee822faa675a613b6
Juan C. Garcia <jcgarciaram@gmail.com>
2016-09-09 06:47:42 -0400

    Added animation and flag support to chapter8/towers-of-hanoi.go

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cracking-the-coding-interview-go
 Solutions to problems in the Cracking the Coding Interview developed in Go
+
+##TODO:
+Chapter 8: towers-of-hanoi.go
+    - Animation does not support number of disks greater than 9
--- chapter8/towers-of-hanoi.go
+++ chapter8/towers-of-hanoi.go
@@ -1,11 +1,32 @@
-// Problem 8.5 Towers of Hanoi
+/* 
+Problem 8.5 Towers of Hanoi
 
-// https://play.golang.org/p/D9cqRB2EPB
+https://play.golang.org/p/D9cqRB2EPB
+
+Usage:
+  -n int
+        number of disks (default 3)
+  -t int
+        delay between moves animation (default 500) 
+*/
 
 package main
 
 import (
 	"fmt"
+    "os"
+    "strconv"
+    "bufio"
+    "time"
+    "os/exec"
+    "flag"
+)
+
+var (
+    f = bufio.NewWriter(os.Stdout)
+    numDisks int
+    numMoves = 0
+    timeDelay int
 )
 
 // Used to be able to define functions for the type []Tower
@@ -26,107 +47,169 @@ type Disk struct {
 }
 
 // Pop a disk from a tower
-func (t *Tower) pop() Disk {
-	d := t.Head
-	t.Head = t.Head.Next
-	t.NumDisks -= 1
+func (ts *TowerSlice) pop(towerIndex int) Disk {
+	d := (*ts)[towerIndex].Head
+	(*ts)[towerIndex].Head = (*ts)[towerIndex].Head.Next
+	(*ts)[towerIndex].NumDisks -= 1
 	return *d
 }
 
 // Push a disk unto a tower
-func (t *Tower) push(d Disk) {
-	if t.NumDisks == 0 {
-		t.Head = &d
-		t.NumDisks += 1
-		t.Tail = &d
+func (ts *TowerSlice) push(d Disk, towerIndex int, printBool bool) {
+    
+	if (*ts)[towerIndex].NumDisks == 0 {
+    
+		(*ts)[towerIndex].Tail = &d
+        
 	} else {
-		d.Next = t.Head
-		t.Head.Previous = &d
-		t.Head = &d
-		t.NumDisks += 1
+		d.Next = (*ts)[towerIndex].Head
+		(*ts)[towerIndex].Head.Previous = &d		
 	}
+    
+    
+    (*ts)[towerIndex].Head = &d
+    (*ts)[towerIndex].NumDisks += 1
+    
+    
+    
+    if printBool {
+        numMoves += 1
+        ts.print(true)
+    }
 }
 
 // Print all towers
-func (towers TowerSlice) print() {
-	for _, t := range towers {
-		d := t.Tail
-		for i := 0; i < t.NumDisks; i++ {
-			fmt.Print(d.Value)
-			fmt.Print(" ")
-			
-			d = d.Previous
-			
-		}
-		fmt.Print("\n")
-	}
-	fmt.Println("------------------")
+func (ts *TowerSlice) print(replace bool) {
+    
+    if replace {overwriteLinesTerminal(numDisks+2)}
+    
+    
+    currDisks := []*Disk{(*ts)[0].Head, (*ts)[1].Head, (*ts)[2].Head}
+    
+    for currLine := numDisks; currLine >= 1;  currLine--{
+        
+        for i := 0; i < 3; i ++ {
+            
+            if (*ts)[i].NumDisks >= currLine {
+                fmt.Print(currDisks[i].Value)
+                currDisks[i] = currDisks[i].Next
+            } else {
+                fmt.Print(" ")
+            }
+            fmt.Print("   ")
+            
+        }
+        fmt.Print("        \n")
+        
+    }
+    fmt.Printf("---------------\nTotal Moves: %d\n", numMoves)
+    time.Sleep(time.Millisecond*time.Duration(timeDelay))
 }
 
 // Function to solve. 
-func towersOfHanoi(towers TowerSlice ) TowerSlice  {
-	towers.print()
-	
-	// If number of disks in first tower is 0, do nothing.
-	if towers[0].NumDisks == 0 {
-		return towers
-	}
+func towersOfHanoi(ts TowerSlice) TowerSlice  {
+	ts.print(false)
+
 	
 	// If number of disks in first tower is one, simply move the disk to last tower.
-	if towers[0].NumDisks == 1 {
-		d := towers[0].pop()
-		towers[2].push(d)
-		towers.print()
-		return towers
+	if ts[0].NumDisks == 1 {
+		d := ts.pop(0)
+		ts.push(d, 2, true)
+		
+		return ts
 	}
 	
 	// If number of disks is at least two, then call makeMove to move the disks from tower 1 to tower 3 using tower 2 and intermediary.
-	towers.makeMove(0, 2, 1, towers[0].NumDisks)
-	return towers
+	ts.makeMove(0, 2, 1, ts[0].NumDisks)
+	return ts
 }
 
 
 // Function to move all disks from towerOrigin to towerDest using towerInterm as intermediary.
 
-func (towers TowerSlice) makeMove(towerOrigin, towerDest, towerInterm, numDisks int) {
+func (towers *TowerSlice) makeMove(towerOrigin, towerDest, towerInterm, numDisks int) {
 	if numDisks == 2 {
-		d := towers[towerOrigin].pop()
-		towers[towerInterm].push(d)
-		towers.print()
-		d = towers[towerOrigin].pop()
-		towers[towerDest].push(d)
-		towers.print()
-		d = towers[towerInterm].pop()
-		towers[towerDest].push(d)
-		towers.print()
+		d := towers.pop(towerOrigin)
+		towers.push(d, towerInterm, true)
+		
+		d = towers.pop(towerOrigin)
+		towers.push(d, towerDest, true)
+		
+		d = towers.pop(towerInterm)
+		towers.push(d, towerDest, true)
+		
 	} else {
 		towers.makeMove(towerOrigin, towerInterm, towerDest, numDisks - 1)
-		d := towers[towerOrigin].pop()
-		towers[towerDest].push(d)
-		towers.print()
+        
+		d := towers.pop(towerOrigin)
+		towers.push(d, towerDest, true)
+		
 		towers.makeMove(towerInterm, towerDest, towerOrigin, numDisks - 1)
 	}
 }
 
+
+func overwriteLinesTerminal(numLines int) {
+    cmd := exec.Command("echo", "-en", "\\e[" + strconv.Itoa(numLines)+ "A")  
+	cmd.Stdout = f    
+    if err := cmd.Run(); err != nil {
+        fmt.Println("Error echoing: ", err)
+    }
+    
+    f.Flush()
+}
+
+
+
+
 func main() {
+    
+    flag.IntVar(&numDisks, "n", 3, "number of disks")
+	flag.IntVar(&timeDelay, "t", 500, "delay between moves animation")
+    
+    flag.Parse()
+    
+    
+    /* var err error
+    if len(os.Args) == 1 {
+        fmt.Println("\nHey! Just FYI, you can define the number of disks to start with by passing an argument for the number of disks you like:\n\nExample: towers-of-hanoi 3\n\nYou can also define the difference in time between moves by defining a secon")
+        
+    } else if len(os.Args) == 2 {
+    
+        if numDisks, err = strconv.Atoi(os.Args[1]); err != nil || numDisks <= 0 {
+            fmt.Println("\nWhoops! The argument passed needs to be a positive number :) ")
+            return
+        }
+        
+        
+        
+        
+    } */
+    
+    fmt.Printf("\nRunning towers of hanoi with %d initial disks\n\n", numDisks)
+        
+        
+        
+        
+    
 	
-	numDisks := 12
 	
 	
 	// Create the towers
-	var towers TowerSlice
+	var ts TowerSlice
 	for i := 0; i < 3; i++ {
 		t := Tower{}
-		towers = append(towers, t)
+		ts = append(ts, t)
 	}
 	
 	// Push initial disks on first tower
 	for i := numDisks; i >= 1; i-- {
 		d := Disk{i, nil, nil}
-		towers[0].push(d)
+		ts.push(d, 0, false)
 	}
 
 	// Solve
-	towers = towersOfHanoi(towers)
-
+	ts = towersOfHanoi(ts)
+    fmt.Println()
 }
+
